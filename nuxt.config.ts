7fbedea226f467188fa8a7a25066fb656e5eec09
mamokosan <mamorin@usubuchinoMacBook-puro.local>
2021-02-08 17:54:01 +0900

    pwa設定変更

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -62,19 +62,22 @@ export default {
   build: {},
 
   manifest: {
-    name: 'Nuxt.jsのPWA',      // アプリの名称
-    short_name: 'Nuxt PWA',    // ホーム画面に表示される名称
+    name: '習慣化',      // アプリの名称
+    short_name: '習慣化',    // ホーム画面に表示される名称
     display: 'standalone',     // 表示モード  'fullscreen' 'standalone' 'minimal-ui' 'browser'
-    theme_color: '#ff4a93',    // アプリケーションの既定のテーマ色を定義
-    background_color: '#ffdce6',  // 背景の色
+    theme_color: 'blue',    // アプリケーションの既定のテーマ色を定義
+    background_color: 'blue',  // 背景の色
     lang: 'ja',                // 言語
-    start_url: '/?mode=pwa',   // アイコンから起動した時のURL
+    start_url: '',   // アイコンから起動した時のURL
     icons: [
       {
-        src: '/static/icon.png',
+        src: '/static/dog.png',
         sizes: '512x512',
         type: 'image/png'
       }
     ],
+  },
+  workbox: {
+    dev: true
   }
 }
